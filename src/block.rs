a1d1d17c84f76b73b794eaac73f613afcad9e525
sunrise <575563079@qq.com>
2022-10-18 03:14:08 +0800

    update

--- src/block.rs
+++ src/block.rs
@@ -1,9 +1,25 @@
 use crate::{error::*, types::*};
 use std::{collections::HashMap, path::Path};
 
-// use reqwest::blocking::{
-//     multipart::{From, Part},
-//     Client,
-// };
+use reqwest::blocking::{
+    multipart::{Form, Part},
+    Client,
+};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Default)]
+pub struct AccountBuilder {
+    access_token: Option<String>,
+    short_name: String,
+    author_name: Option<String>,
+    author_url: Option<String>,
+}
+
+impl AccountBuilder{
+
+}
+
+
 
 
